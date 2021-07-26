e3d1abbecd3ce4b6af5094fbb5b384b49bd23980
appproach <ishan.sharma10001@gmail.com>
2021-07-26 08:11:32 +0530

    Initial Set Up

---
+++ src/test/java/com/fluxandmonoplayground/FluxAndMonoTest.java
@@ -0,0 +1,43 @@
+package com.fluxandmonoplayground;
+
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class FluxAndMonoTest {
+
+    @Test
+    public void fluxTest()
+    {
+        Flux<String> stringFlux=Flux.just("String","String Builder","String Buffer").log();
+        stringFlux.subscribe(System.out::println,(e)->System.err.println("Error"),()-> System.out.println("Completed"));
+
+    }
+
+    @Test
+    public void fluxTestWithoutError()
+    {
+        Flux<String> stringFlux=Flux.just("String","String Builder","String Buffer").log();
+        StepVerifier.create(stringFlux)
+                .expectNext("String")
+                .expectNext("String Builder")
+                .expectNext("String Buffer")
+                .verifyComplete();
+    }
+    @Test
+    public void fluxTestWithError()
+    {
+        Flux<String> stringFlux=Flux.just("String","String Builder","String Buffer")
+                .concatWith(Flux.error(new RuntimeException("Error Occured")))
+                .log();
+        StepVerifier.create(stringFlux)
+                .expectNextCount(3)
+                .expectNext("String","String Builder","String Buffer")
+                .expectNext("String")
+                .expectNext("String Builder")
+                .expectNext("String Buffer")
+                .expectErrorMessage("Error Occured")
+                /*.expectError(RuntimeException.class)*/
+                .verify();
+    }
+}
